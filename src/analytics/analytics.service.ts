e68e16c26c23b45462087dcc21a675f0a3a51000
JKusio <jakubkusiowski@gmail.com>
2025-02-17 11:40:41 +0100

    Add analytics service

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { BinanceService } from 'src/binance/binance.service';
-import { AnalysisResultDto } from './dto/analysis-result.dto';
+import {
+  AnalysisResultDto,
+  PeriodDto,
+  PeriodType,
+} from './dto/analysis-result.dto';
 import { AnalyzeCryptoDataDto } from './dto/analyze-crypto-data.dto';
 
 @Injectable()
@@ -16,10 +20,97 @@ export class AnalyticsService {
       endTime: new Date(dto.endTime),
     });
 
-    console.log(historicalData);
+    let topPrice = historicalData[0].price;
+    let lowestPrice = historicalData[0].price;
+    const periods: Array<PeriodDto> = [];
+
+    let periodStartPrice = historicalData[0].price;
+    let periodStartTime = historicalData[0].timestamp;
+    let periodType: PeriodType | null = null;
+
+    let previousPrice = historicalData[0].price;
+    let previousTimestamp = historicalData[0].timestamp;
+
+    historicalData.splice(0, 1);
+
+    for (const { price, timestamp } of historicalData) {
+      if (topPrice < price) {
+        topPrice = price;
+      }
+
+      if (lowestPrice > price) {
+        lowestPrice = price;
+      }
+
+      if (periodType === null) {
+        if (price === periodStartPrice) {
+          periodType = PeriodType.STB;
+        } else if (price > periodStartPrice) {
+          periodType = PeriodType.INC;
+        } else if (price < periodStartPrice) {
+          periodType = PeriodType.DEC;
+        }
+      } else {
+        if (periodType === PeriodType.STB) {
+          if (price !== periodStartPrice) {
+            periods.push({
+              startPrice: periodStartPrice,
+              endPrice: price,
+              startTime: periodStartTime,
+              endTime: timestamp,
+              periodType,
+            });
+
+            periodType =
+              price > periodStartPrice ? PeriodType.INC : PeriodType.DEC;
+
+            periodStartPrice = price;
+            periodStartTime = timestamp;
+          }
+        } else if (periodType === PeriodType.INC) {
+          if (price <= previousPrice) {
+            periods.push({
+              startPrice: periodStartPrice,
+              endPrice: price,
+              startTime: periodStartTime,
+              endTime: timestamp,
+              periodType,
+            });
+
+            periodType =
+              price === previousPrice ? PeriodType.STB : PeriodType.DEC;
+
+            periodStartPrice = previousPrice;
+            periodStartTime = previousTimestamp;
+          }
+        } else if (periodType === PeriodType.DEC) {
+          if (price >= previousPrice) {
+            periods.push({
+              startPrice: periodStartPrice,
+              endPrice: price,
+              startTime: periodStartTime,
+              endTime: timestamp,
+              periodType,
+            });
+
+            periodType =
+              price === previousPrice ? PeriodType.STB : PeriodType.INC;
+
+            periodStartPrice = previousPrice;
+            periodStartTime = previousTimestamp;
+          }
+        }
+      }
+
+      previousPrice = price;
+      previousTimestamp = timestamp;
+    }
 
     return {
-      id: 1,
+      symbol: dto.symbol,
+      topPrice,
+      lowestPrice,
+      periods,
     };
   }
 }
--- src/analytics/dto/analysis-result.dto.ts
+++ src/analytics/dto/analysis-result.dto.ts
@@ -1,3 +1,20 @@
+export enum PeriodType {
+  INC = 'INC',
+  DEC = 'DEC',
+  STB = 'STB',
+}
+
+export type PeriodDto = {
+  startPrice: string;
+  endPrice: string;
+  startTime: number;
+  endTime: number;
+  periodType: PeriodType;
+};
+
 export type AnalysisResultDto = {
-  id: number;
+  symbol: string;
+  topPrice: string;
+  lowestPrice: string;
+  periods: Array<PeriodDto>;
 };
